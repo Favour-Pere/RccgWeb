577c0679ba4e9f39dd4d0e7f38c13aef4f473841
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-02-11 00:51:33 +0100

    Battery about to die

--- RccgWeb/Controllers/ProgramActivityController.cs
+++ RccgWeb/Controllers/ProgramActivityController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RccgWeb.Data;
+using RccgWeb.Models;
 
 namespace RccgWeb.Controllers
 {
@@ -7,5 +9,46 @@ namespace RccgWeb.Controllers
     [ApiController]
     public class ProgramActivityController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProgramActivityController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("AddActivity")]
+        public async Task<IActionResult> AddActivity([FromBody] ProgramActivity programActivity)
+        {
+            if (programActivity == null)
+            {
+                return BadRequest(new
+                {
+                    message = "Invalid activity data"
+                });
+            }
+
+            try
+            {
+                int idCount = new[]
+                {
+                    programActivity.ZoneId,
+                    programActivity.AreaId,
+                    programActivity.ParishId
+                }.Count(id => id.HasValue);
+
+                if (idCount != 1)
+                {
+                    return BadRequest(new
+                    {
+                        message = "Program Activity must be linked to one of zone, Area, or parish."
+                    });
+                }
+
+                if (programActivity.ZoneId.HasValue)
+                {
+                    programActivity.ChurchId = _context.Zones.Where
+                }
+            }
+        }
     }
 }
