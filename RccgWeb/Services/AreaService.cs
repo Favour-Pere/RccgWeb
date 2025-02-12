868110aebc9981931ab35db6831ba391afd3cc6d
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-02-13 00:23:45 +0100

    implemented the AreaService interface

---
+++ RccgWeb/Services/AreaService.cs
@@ -0,0 +1,36 @@
+﻿using RccgWeb.Data;
+using RccgWeb.Models;
+using RccgWeb.Services.Interfaces;
+
+namespace RccgWeb.Services
+{
+    public class AreaService : IAreaService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AreaService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddAreaAsync(Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Area> GetAreaByIdAsync(Guid areadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Area>> GetAreasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAreaAsync(Area area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
