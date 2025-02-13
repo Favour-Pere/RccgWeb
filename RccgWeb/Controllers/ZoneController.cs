b48a1c838516b17627ae9aaf256741174734aafd
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-02-13 23:16:56 +0100

    Introducing View Models

--- RccgWeb/Controllers/ZoneController.cs
+++ RccgWeb/Controllers/ZoneController.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RccgWeb.Data;
 using RccgWeb.Models;
 
 namespace RccgWeb.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ZoneController : ControllerBase
+    public class ZoneController : Controller
     {
         private readonly ApplicationDbContext _context;
 
@@ -16,28 +15,81 @@ namespace RccgWeb.Controllers
             _context = context;
         }
 
-        [HttpPost("create")]
-        public async Task<IActionResult> CreateZone()
+        public async Task<IActionResult> Index()
         {
-            var newZone = new Zone
-            {
-                ZoneId = Guid.NewGuid(),
-                ChurchId = ChurchIdGenerator.GenerateChurchId(_context),
-                ZoneName = "Auto Zone",
-                ZonePastor = "Pastor Auto",
-                DateCreated = DateTime.Now,
-                Location = "Auto Location"
-            };
+            var zones = await _context.Zones.ToListAsync();
 
-            _context.Zones.Add(newZone);
-
-            await _context.SaveChangesAsync();
-
-            return Ok(new
-            {
-                message = "Zone added successfully!",
-                data = newZone
-            });
+            return View(zones);
         }
+
+        public IActionResult AddZone()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddZone(Zone zone)
+        {
+            if (ModelState.IsValid)
+            {
+                zone.ZoneId = Guid.NewGuid();
+                zone.ChurchId = ChurchIdGenerator.GenerateChurchId(_context);
+                zone.DateCreated = DateTime.Now;
+
+                _context.Zones.Add(zone);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(zone);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var zone = await _context.Zones.FirstOrDefaultAsync(z => z.ZoneId == id);
+
+            if (zone == null)
+            {
+                return NotFound();
+            }
+
+            return View(zone);
+        }
+
+        public async Task<IActionResult> AreasUnderZone(Guid id)
+        {
+            var areas = await _context.Areas.Where(a => a.ZoneId == id).ToListAsync();
+
+            ViewBag.ZoneId = id;
+
+            return View(areas);
+        }
+
+        //[HttpPost("create")]
+        //public async Task<IActionResult> CreateZone()
+        //{
+        //    var newZone = new Zone
+        //    {
+        //        ZoneId = Guid.NewGuid(),
+        //        ChurchId = ChurchIdGenerator.GenerateChurchId(_context),
+        //        ZoneName = "Auto Zone",
+        //        ZonePastor = "Pastor Auto",
+        //        DateCreated = DateTime.Now,
+        //        Location = "Auto Location"
+        //    };
+
+        //    _context.Zones.Add(newZone);
+
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(new
+        //    {
+        //        message = "Zone added successfully!",
+        //        data = newZone
+        //    });
+        //}
     }
 }
---
+++ RccgWeb/ViewModel/ZoneViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RccgWeb.ViewModel
+{
+    public class ZoneViewModel
+    {
+        [Required]
+        [Display(Name = "Name of Zone")]
+        public string ZoneName { get; set; }
+
+        [Required]
+        [Display(Name = "Pastor in charge of Zone")]
+        public string ZonePastor { get; set; }
+
+        [Required]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+    }
+}
