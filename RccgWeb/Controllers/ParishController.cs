32ab5c327756b35d617b91873ee027facf88d3f4
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-02-18 22:12:26 +0100

    Finished the Parish

--- RccgWeb/Controllers/ParishController.cs
+++ RccgWeb/Controllers/ParishController.cs
@@ -1,19 +1,90 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using RccgWeb.Data;
 using RccgWeb.Models;
+using RccgWeb.ViewModel;
 
 namespace RccgWeb.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ParishController : ControllerBase
+    public class ParishController : Controller
     {
         private readonly ApplicationDbContext _context;
 
         public ParishController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var parishes = await _context.Parishes.ToListAsync();
+
+            return View(parishes);
+        }
+
+        public async Task<IActionResult> AddParish()
+        {
+            var areas = await _context.Areas.Select(a => new SelectListItem
+            {
+                Value = a.AreaId.ToString(),
+                Text = a.AreaName
+            }).ToListAsync();
+
+            var model = new ParishViewModel
+            {
+                Areas = areas
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddParish(ParishViewModel parishViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var parish = new Parish
+                {
+                    ParishId = Guid.NewGuid(),
+                    ChurchId = ChurchIdGenerator.GenerateChurchId(_context),
+                    ParishName = parishViewModel.ParishName,
+                    ParishPastor = parishViewModel.ParishPastor,
+                    DateCreated = DateTime.Now,
+                    Location = parishViewModel.Location,
+                    AreaId = parishViewModel.AreaId
+                };
+
+                await _context.Parishes.AddAsync(parish);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            parishViewModel.Areas = await _context.Areas.Select(a => new SelectListItem
+            {
+                Value = a.AreaId.ToString(),
+                Text = a.AreaName
+            }).ToListAsync();
+
+            return View(parishViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var parish = await _context.Parishes.FindAsync(id);
+
+            if (parish == null)
+            {
+                return NotFound();
+            }
+
+            return View(parish);
         }
 
         [HttpPost("create")]
---
+++ RccgWeb/ViewModel/ParishViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace RccgWeb.ViewModel
+{
+    public class ParishViewModel
+    {
+        [Required(ErrorMessage = "Parish name is required")]
+        [Display(Name = "Parish Name")]
+        public string ParishName { get; set; }
+
+        [Required(ErrorMessage = "Parish Pastor is required")]
+        [Display(Name = "Parish Pastor")]
+        public string ParishPastor { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        public Guid AreaId { get; set; }
+
+        public List<SelectListItem>? Areas { get; set; }
+    }
+}
