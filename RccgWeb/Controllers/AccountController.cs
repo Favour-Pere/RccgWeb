d2d1c97015d9caa3715b3b8c1dff9f376d20389c
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-02-19 23:55:52 +0100

    Added some view models

--- RccgWeb/Controllers/AccountController.cs
+++ RccgWeb/Controllers/AccountController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RccgWeb.Models;
+using RccgWeb.ViewModel;
 
 namespace RccgWeb.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
+
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    ChurchId = model.ChurchId
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password);
+            }
+        }
     }
-}
---
+++ RccgWeb/ViewModel/ChurchAssignmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RccgWeb.ViewModel
+{
+    public class ChurchAssignmentViewModel
+    {
+        public string UserId { get; set; } = default!;
+
+        public string ChurchId { get; set; } = default!;
+
+        public Guid? ZoneId { get; set; }
+
+        public Guid? AreaId { get; set; }
+
+        public Guid? ParishId { get; set; }
+    }
+}
