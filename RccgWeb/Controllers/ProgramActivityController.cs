08004b38b05dfdebfeae4f494a33a69b254e4dac
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-03-02 00:08:58 +0100

    Update ProgramActivityController.cs

--- RccgWeb/Controllers/ProgramActivityController.cs
+++ RccgWeb/Controllers/ProgramActivityController.cs
@@ -24,22 +24,31 @@ namespace RccgWeb.Controllers
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
-            if (user is not null)
-            {
-                var userHasChurch = await _context.UserChurches.AnyAsync(uc => uc.UserId == user.Id.ToString());
-
-                if (!userHasChurch)
-                {
-                    TempData["Message"] = "You need to be assigned to a church before adding activities. Please Contanct an administrator";
-
-                    return View("Index");
-                }
-            }
-            else
+            if (user is  null)
             {
                 return RedirectToAction("Login", "Account");
             }
-            var activities = await _context.ProgramActivities.ToListAsync();
+
+            var userHasChurch = await _context.UserChurches.FirstOrDefaultAsync(uc => uc.UserId == user.Id.ToString());
+
+            if (userHasChurch == null)
+            {
+                TempData["Message"] = "You need to be assigned to a church before adding activities. Please Contanct an administrator";
+
+                return View("Index");
+            }
+
+            //if (!userHasChurch)
+            //{
+            //    TempData["Message"] = "You need to be assigned to a church before adding activities. Please Contanct an administrator";
+
+            //    return View("Index");
+            //}
+
+            var activities = await _context.ProgramActivities
+                    .Where(pa => pa.ChurchId == userHasChurch.ChurchId)
+                    .OrderByDescending(pa => pa.DateTimeSubmitted) // Assuming there's a CreatedAt field
+                    .ToListAsync();
 
             return View(activities);
         }
@@ -111,49 +120,5 @@ namespace RccgWeb.Controllers
             return RedirectToAction("Index");
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> AddActivity([FromBody] ProgramActivity programActivity)
-        //{
-        //    if (programActivity == null || string.IsNullOrEmpty(programActivity.ChurchId))
-        //    {
-        //        return BadRequest(new { message = "Invalid activity data. ChurchId is required." });
-        //    }
-
-        //    try
-        //    {
-        //        // Check for existing Zone, Area, or Parish with the given ChurchId
-        //        var existingChurch = await _context.Zones.AsNoTracking().FirstOrDefaultAsync(z => z.ChurchId == programActivity.ChurchId)
-        //                          ?? (object)await _context.Areas.AsNoTracking().FirstOrDefaultAsync(a => a.ChurchId == programActivity.ChurchId)
-        //                          ?? (object)await _context.Parishes.AsNoTracking().FirstOrDefaultAsync(p => p.ChurchId == programActivity.ChurchId);
-
-        //        if (existingChurch == null)
-        //        {
-        //            return NotFound(new { message = "No Zone, Area, or Parish found for the provided ChurchId." });
-        //        }
-
-        //        // Ensure DateCreated is properly set
-        //        if (programActivity.DateCreated == default)
-        //        {
-        //            programActivity.DateCreated = DateTime.Now;
-        //        }
-
-        //        _context.ProgramActivities.Add(programActivity);
-        //        await _context.SaveChangesAsync();
-
-        //        return Ok(new
-        //        {
-        //            message = "Program activity added successfully",
-        //            data = programActivity
-        //        });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, new
-        //        {
-        //            message = "An error occurred while adding the activity.",
-        //            error = ex.Message
-        //        });
-        //    }
-        //}
     }
 }
