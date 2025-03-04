e36a794802fa3ad95efb7ba7c9b0c4903efb35b0
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-03-04 21:09:31 +0100

    Create ActivityViewModel.cs

---
+++ RccgWeb/ViewModel/ActivityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RccgWeb.Models;
+
+namespace RccgWeb.ViewModel
+{
+    public class ActivityViewModel
+    {
+        public string ChurchName { get; set; } // Name of the assigned church
+        public string Location { get; set; } // Church Location
+        public DateTime? DateCreated { get; set; } // Date the Church was created
+        public List<ProgramActivity> Activities { get; set; } // List of activities
+    }
+}
