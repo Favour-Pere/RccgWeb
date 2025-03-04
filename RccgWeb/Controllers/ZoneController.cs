b472671073ab8762faf41bd31561d58619e5f3eb
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-03-04 21:10:33 +0100

    Update ZoneController.cs

--- RccgWeb/Controllers/ZoneController.cs
+++ RccgWeb/Controllers/ZoneController.cs
@@ -77,29 +77,5 @@ namespace RccgWeb.Controllers
 
             return View(areas);
         }
-
-        //[HttpPost("create")]
-        //public async Task<IActionResult> CreateZone()
-        //{
-        //    var newZone = new Zone
-        //    {
-        //        ZoneId = Guid.NewGuid(),
-        //        ChurchId = ChurchIdGenerator.GenerateChurchId(_context),
-        //        ZoneName = "Auto Zone",
-        //        ZonePastor = "Pastor Auto",
-        //        DateCreated = DateTime.Now,
-        //        Location = "Auto Location"
-        //    };
-
-        //    _context.Zones.Add(newZone);
-
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok(new
-        //    {
-        //        message = "Zone added successfully!",
-        //        data = newZone
-        //    });
-        //}
     }
 }
