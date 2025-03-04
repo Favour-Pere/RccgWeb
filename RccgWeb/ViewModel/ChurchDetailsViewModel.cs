5dab6c718dcd156755e983c9173b2f11590f62ca
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-03-04 21:10:39 +0100

    Create ChurchDetailsViewModel.cs

---
+++ RccgWeb/ViewModel/ChurchDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RccgWeb.ViewModel
+{
+    public class ChurchDetailsViewModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
