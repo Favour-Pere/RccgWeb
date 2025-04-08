c37e5e37efdffc843ec9fc8ef2068dbfa307f704
Robert-Wilson Peremobowei Favour <103349217+Favour-Pere@users.noreply.github.com>
2025-04-08 22:17:10 +0100

    Create ChurchStatsViewModel.cs

---
+++ RccgWeb/ViewModel/ChurchStatsViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RccgWeb.ViewModel;
+
+public class ChurchStatsViewModel
+{
+        public string ChurchId { get; set; } = string.Empty;
+    public string ChurchName { get; set; } = string.Empty;
+
+    public Dictionary<string, decimal> MonthlyOfferings { get; set; } = new();
+    // public Dictionary<string, decimal> MonthlyTithes { get; set; } = new();
+    // public Dictionary<string, int> MonthlyAttendance { get; set; } = new();
+
+    // public double OfferingGrowthRate { get; set; }
+
+    public int Year { get; set; }
+    public int Month { get; set; }
+}
+
